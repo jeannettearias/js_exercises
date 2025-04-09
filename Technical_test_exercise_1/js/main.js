74232666c9ad5de7cac49ced33a701652612ad15
jeannettearias <jeannettearias@icloud.com>
2025-04-09 23:42:18 +0200

    solved

--- Technical_test_exercise_1/js/main.js
+++ Technical_test_exercise_1/js/main.js
@@ -1,5 +1,8 @@
-const numbersArray = [1, 2, 3, 4, 5];
-if (numbersArray.length = 100) {
-    return numbersArray;
-}
-console.log(numbersArray);
+const numbersArray = [1, 2, 3, 4, 5, 20, 34, 56, 78, 90, 100];
+numbersArray.sort((function (a, b) {
+    return a - b;
+
+}));
+//length = cantidad de elementos del array
+console.log(numbersArray[numbersArray.length - 1]);
+
