7ca7a18c93633dba307fb8b4f26f63baad05a3f6
jeannettearias <jeannettearias@icloud.com>
2025-05-05 19:45:33 +0200

    starting fifth exercise

---
+++ technical_test_exercise_5/js/main.js
@@ -0,0 +1,18 @@
+/*
+Se necesita que haga una función que reciba como parámetro un objeto que
+representa una carpeta de un proyecto. Ese objeto tiene las propiedades:
+- name
+- type
+- children
+La propiedad name es un string. La propiedad type es un string que puede tomar los
+valores “directory” o “file”. La propiedad children será un array de objetos del
+mismo tipo que el expuesto.
+El objeto inicial del parámetro siempre será de tipo “directory”. Puede haber varios
+niveles de profundidad en la estructura (es decir, puede haber un objeto en
+“children” que sea de tipo “directory” y tenga a su vez otros “children”).
+Se desea que la función escriba en la consola (o en la terminal) el listado de todos los
+contenidos de la carpeta de proyecto indentados.
+Ejemplo: Para el objeto de este enlace Deberá mostrar lo mismo que este enlace
+
+
+*/
